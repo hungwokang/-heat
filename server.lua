7eafdce731a81d7892e0f15313e704aad01beef8
hungwokang <hungwokang@gmail.com>
2025-10-25 06:31:43 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -9,7 +9,7 @@ local hed = char.Head
 local hes = game.Players.LocalPlayer.Character.Humanoid
 
 Class_Name=string.reverse"ihS-ihS yB tidE "
--- Edit more !yesss
+-- Edit more !yessss
 
 
 local player = game:GetService('Players').LocalPlayer
